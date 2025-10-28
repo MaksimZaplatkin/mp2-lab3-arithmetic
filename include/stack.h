c4efe851a7e6df7ac6439cf022823fe2a3bf384b
MaksimZaplatkin <berikgulzu@gmail.com>
2025-10-28 19:37:48 +0000

    stack

--- include/stack.h
+++ include/stack.h
@@ -1,9 +1,119 @@
-// объявление и реализация шаблонного стека
-// стек поддерживает операции: 
-// - вставка элемента, 
-// - извлечение элемента, 
-// - просмотр верхнего элемента (без удаления)
-// - проверка на пустоту, 
-// - получение количества элементов в стеке
-// - очистка стека
-// при вставке в полный стек должна перевыделяться память
+// РѕР±СЉСЏРІР»РµРЅРёРµ Рё СЂРµР°Р»РёР·Р°С†РёСЏ С€Р°Р±Р»РѕРЅРЅРѕРіРѕ СЃС‚РµРєР°
+// СЃС‚РµРє РїРѕРґРґРµСЂР¶РёРІР°РµС‚ РѕРїРµСЂР°С†РёРё: 
+// - РІСЃС‚Р°РІРєР° СЌР»РµРјРµРЅС‚Р°, +
+// - РёР·РІР»РµС‡РµРЅРёРµ СЌР»РµРјРµРЅС‚Р°, +
+// - РїСЂРѕСЃРјРѕС‚СЂ РІРµСЂС…РЅРµРіРѕ СЌР»РµРјРµРЅС‚Р° (Р±РµР· СѓРґР°Р»РµРЅРёСЏ)+
+// - РїСЂРѕРІРµСЂРєР° РЅР° РїСѓСЃС‚РѕС‚Сѓ, +
+// - РїРѕР»СѓС‡РµРЅРёРµ РєРѕР»РёС‡РµСЃС‚РІР° СЌР»РµРјРµРЅС‚РѕРІ РІ СЃС‚РµРєРµ+
+// - РѕС‡РёСЃС‚РєР° СЃС‚РµРєР°+
+// РїСЂРё РІСЃС‚Р°РІРєРµ РІ РїРѕР»РЅС‹Р№ СЃС‚РµРє РґРѕР»Р¶РЅР° РїРµСЂРµРІС‹РґРµР»СЏС‚СЊСЃСЏ РїР°РјСЏС‚СЊ
+
+template <typename T>
+class Stack
+{
+	int filled;
+	int size;
+	T* mem;
+public:
+	Stack();
+	Stack(int s);
+	Stack(const Stack<T>&);
+	~Stack();
+	bool Is_Empty()
+	{
+		if (filled == 0)
+		{
+			return true;
+		}
+		return false;
+	}
+	int How_Many_Elem()
+	{
+		return filled;
+	}
+	void Clear()
+	{
+		if (!Is_Empty())
+		{
+			filled = 0;
+			delete[]mem;
+			mem = new T[size];
+		}
+	}
+	T Top();
+	T Pop();
+	void Push(T el);
+};
+
+template <typename T>
+Stack<T>::Stack(int s)
+{
+	filled = 0;
+	size = s;
+	mem = new T[size];
+}
+
+template <typename T>
+Stack<T>::Stack()
+{
+	filled = 0;
+	size = 100;
+	mem = new T[100];
+}
+
+template <typename T>
+Stack<T>::Stack(const Stack<T>& OtS)
+{
+	filled = OtS.filled;
+	size = OtS.size;
+	mem = new T[size];
+	for (int i = 0; i < filled; i++)
+	{
+		mem[i] = OtS.mem[i];
+	}
+}
+
+template <typename T>
+Stack<T>::~Stack()
+{
+	delete[]mem;
+}
+
+template <typename T>
+T Stack<T>::Top()
+{
+	if (Is_Empty())
+	{
+		throw std::logic_error("Empty_Stack_Top_Element");
+	}
+	return mem[filled - 1];
+}
+
+template <typename T>
+T Stack<T>::Pop()
+{
+	if (Is_Empty())
+	{
+		throw std::logic_error("Empty_Stack_Top_Element");
+	}
+	filled -= 1;
+	return mem[filled];
+}
+
+template <typename T>
+void Stack<T>::Push(T el)
+{
+	if (filled == size)
+	{
+		T* tmp = new T[size * 2];
+		for (int i = 0; i < filled; i++)
+			tmp[i] = mem[i];
+		delete[] mem;
+		size = size * 2;
+		mem = new T[size];
+		for (int i = 0; i < filled; i++)
+			mem[i] = tmp[i];
+	}
+	mem[filled] = el;
+	filled++;
+}
--- test/test_stack.cpp
+++ test/test_stack.cpp
@@ -1,4 +1,108 @@
-// тесты для стека
+// С‚РµСЃС‚С‹ РґР»СЏ СЃС‚РµРєР°
 
 #include "stack.h"
 #include <gtest.h>
+
+TEST(Stack, can_create_stack)
+{
+	ASSERT_NO_THROW(Stack <int> a);
+}
+
+TEST(Stack, can_create_stack_with_positive_lenght)
+{
+	ASSERT_NO_THROW(Stack <int> a(5));
+}
+
+TEST(Stack, can_not_create_stack_with_negativ_length)
+{
+	ASSERT_ANY_THROW(Stack <int> a(-10));
+}
+
+TEST(Stack, can_copy_stack)
+{
+	Stack <int> a(8);
+	a.Push(4);
+	a.Push(6);
+	ASSERT_NO_THROW(Stack <int> b(a));
+}
+
+TEST(Stack, can_add_element)
+{
+	Stack <int> a(10);
+	int b = 1;
+	ASSERT_NO_THROW(a.Push(b));
+}
+
+TEST(Stack, can_add_element_to_full_stack)
+{
+	Stack <int> a(1);
+	a.Push(3);
+	int b = 1;
+	ASSERT_NO_THROW(a.Push(b));
+}
+
+TEST(Stack, stacks_is_empty)
+{
+	Stack<int> a(10);
+	EXPECT_TRUE(a.Is_Empty());
+}
+
+
+TEST(Stack, can_pop_element)
+{
+	Stack<int> a(10);
+	int b = 3;
+	a.Push(b);
+	EXPECT_EQ(b, a.Pop());
+}
+
+TEST(Stack, can_top_element)
+{
+
+	Stack<int> a(10);
+	int b = 1;
+	int c;
+	a.Push(b);
+	c = a.Top();
+	EXPECT_EQ(b, a.Pop());
+}
+
+TEST(Stack, can_not_pop_from_empty_stack)
+{
+	Stack<int> a(10);
+	int b;
+	ASSERT_ANY_THROW(b = a.Pop());
+}
+
+TEST(Stack, can_not_top_from_empty_stack)
+{
+	Stack<int> a(10);
+	int b;
+	ASSERT_ANY_THROW(b = a.Top());
+}
+
+TEST(Stack, can_know_number_of_elements)
+{
+	Stack<int> a(3);
+	int b;
+	for (int i = 0; i < 3; i++)
+	{
+		b = 3 * i + 1;
+		a.Push(b);
+	}
+	EXPECT_EQ(3, a.How_Many_Elem());
+}
+
+TEST(Stack, can_clear_stack)
+{
+	Stack<int> a(3);
+	int b;
+	for (int i = 0; i < 3; i++)
+	{
+		b = 3 * i + 1;
+		a.Push(b);
+	}
+	a.Clear();
+	EXPECT_EQ(0, a.How_Many_Elem());
+}
+
